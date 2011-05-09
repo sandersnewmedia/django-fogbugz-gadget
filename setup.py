0bafc55b2dbca4f0f238b57dfff840f0931ddee1
Scott Meisburger <smeisburger@gmail.com>
2011-05-09 18:05:21 -0500

    added setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='???',
+    version='1.0',
+    description='Django + FogBugz',
+    author='Sanders New Media',
+    author_email='hello@sandersnewmedia.com',
+    url='http://github.com/sandersnewmedia/fogbugz',
+    license='MIT',
+)
